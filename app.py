71816e721f58abf1bad330a5cafeff6a4fc4357f
khc033 <khc033@gmail.com>
2021-09-06 16:42:52 -0700

    Scan runs using flask app

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, flash, request, url_for, redirect, jsonify, render_template
 import urllib.request
 from werkzeug.utils import secure_filename
+from scan import DocScanner
 
 import json
 import os
+import argparse
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'JPG'])
 UPLOAD_FOLDER = os.path.join('static', 'output')
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "secret key"
@@ -23,33 +26,55 @@ def add_headers(response):
     return response
 
 
-@app.route("/")
-def upload_form():
-    return render_template('upload.html')
+# @app.route("/")
+# def upload_form():
+#     return render_template('upload.html')
 
 @app.route('/', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+def get_path(file_path):
+
+	if allowed_file(file_path):
+		filename = secure_filename(file_path)
+		scanner = DocScanner()
 		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		return render_template('upload.html', filename=filename)
+		print('Image successfully uploaded and displayed below')
+		return scanner.scan(filename)
 	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
+		print('Allowed image types are -> png, jpg, jpeg, gif')
+
+
 
 @app.route('/display/<filename>')
 def display_image(filename):
 	#print('display_image filename: ' + filename)
-	return redirect(url_for('static', filename='output/' + filename), code=301)
+	return redirect(url_for('static', filename='outpu0t/' + filename), code=301)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+  ap = argparse.ArgumentParser()
+  group = ap.add_mutually_exclusive_group(required=True)
+  group.add_argument("--images", help="Directory of images to be scanned")
+  group.add_argument("--image", help="Path to single image to be scanned")
+  ap.add_argument("-i", action='store_true',
+      help = "Flag for manually verifying and/or setting document corners")
+
+  args = vars(ap.parse_args())
+  im_dir = args["images"]
+  im_file_path = args["image"]
+  interactive_mode = args["i"]
+  
+  scanner = DocScanner(interactive_mode)
+
+  valid_formats = [".jpg", ".jpeg", ".jp2", ".png", ".bmp", ".tiff", ".tif"]
+
+  get_ext = lambda f: os.path.splitext(f)[1].lower()
+
+  # Scan single image specified by command line argument --image <IMAGE_PATH>
+  if im_file_path:
+      scanner.scan(im_file_path)
+
+  # Scan all valid images in directory specified by command line argument --images <IMAGE_DIR>
+  else:
+      im_files = [f for f in os.listdir(im_dir) if get_ext(f) in valid_formats]
+      for im in im_files:
+          scanner.scan(im_dir + '/' + im)
+  # app.run(debug=True, host='0.0.0.0', port=5000)
